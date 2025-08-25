36c0013f73dd198d3a019739d2a0705526358320
bloger001 <offixcialbloger@gmail.com>
2025-08-25 23:44:07 +0100

    Update firebase.js

--- assets/firebase.js
+++ assets/firebase.js
@@ -3,7 +3,7 @@ window.firebaseConfig = {
   apiKey: "AIzaSyBRGM431CHZ3UMUHIc4Q-S1aGDMfrbu7Gs",
   authDomain: "ican-kit-prep.firebaseapp.com",
   projectId: "ican-kit-prep",
-  storageBucket: "ican-kit-prep.firebasestorage.app",
+  storageBucket: "ican-kit-prep.firebasestorage.app",  // confirm in console
   messagingSenderId: "354385037521",
   appId: "1:354385037521:web:f3a7265f66983942581df0",
   measurementId: "G-LN8E2R4B7X"
