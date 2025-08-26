e38e51140dc8b603b249390b4cf5a28dd6749313
bloger001 <offixcialbloger@gmail.com>
2025-08-26 14:09:23 +0100

    Update firebase.js

--- assets/firebase.js
+++ assets/firebase.js
@@ -5,7 +5,7 @@
     authDomain: "ican-kit-prep.firebaseapp.com",
     databaseURL: "https://ican-kit-prep-default-rtdb.firebaseio.com",
     projectId: "ican-kit-prep",
-    storageBucket: "ican-kit-prep.appspot.com",  // fixed bucket
+    storageBucket: "ican-kit-prep.appspot.com",
     messagingSenderId: "354385037521",
     appId: "1:354385037521:web:f3a7265f66983942581df0",
     measurementId: "G-LN8E2R4B7X"
